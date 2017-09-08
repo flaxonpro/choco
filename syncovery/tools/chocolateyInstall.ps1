b32de424ad6f7eb27ed39257c5c287c131491abd
Cosmo Martinez <cosmo@ucsc.edu>
2017-09-08 14:32:26 -0700

    Add files via upload

---
+++ syncovery/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName            = 'syncovery'
+  fileType               = 'exe'
+  url                    = 'https://www.syncovery.com/release/SyncoverySetup.exe'
+  url64bit               = 'https://www.syncovery.com/release/Syncovery64Setup.exe'
+  checksum               = 'a4d91c73ad8720ae47a4d001fef3b9d103c53a15720c5c88c4890f031b80f7b6'
+  checksum64             = 'd2d6f45216cf188be167d4d0cb0c605bbd6d0202425177fa85f3cffea9b304a6'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/VERYSILENT'
+  validExitCodes         = @(0)
+  softwareName           = 'syncovery*'
+}
+Install-ChocolateyPackage @packageArgs
---
+++ syncovery/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$releases = 'https://www.syncovery.com/download/windows/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $url32   = $download_page.links | ? href -match '.exe$' | % href | select -First 1
+    $url64   = $url32 -replace 'Setup.exe$', '64Setup.exe'
+    $version = $download_page.ParsedHtml.getElementsByTagName("span") | ? innerHTML -match '^Download Syncovery' | % innerHTML | select -First 1 | %{$_.Split(" ")} | select -Index 2
+	$version = $version -split "(?:(?i)(?<=^|\d)(?=[a-z])|(?<=[a-z])(?=$|\d))"
+	$version = $version[0] + "." + [byte][char]$version[1]
+
+    @{
+        URL32   = 'https://www.syncovery.com' + $url32
+        URL64   = 'https://www.syncovery.com' + $url64
+        Version = $version
+    }
+}
+
+update
